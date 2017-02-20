82da55a643fde887b693d9963730f08bc3481168
joker314 <maxzuba22@yahoo.co.uk>
2017-02-20 02:31:44 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+class Person():
+    def __init__(self, knaveOrKnight):
+        self.am = int(knaveOrKnight)
+    
+    def said(self, condition):
+        return condition if self.am else not condition
+
+    def isKnave(self):
+        return not self.am
+
+    def isKnight(self):
+        return self.am
+
+    def __eq__(self, person):
+        return self.am == person.am
+
+    def __ne__(self, person):
+        return self.am != person.am
+
+def calc(num, keys):
+    out = []
+    for i in map(lambda x: (num + 2 - len(bin(x))) * "0" + bin(x)[2:], range(2**num)):
+        yey = True
+        for j in keys:
+            if not j(map(Person, i)):
+               yey = False
+               break
+        if yey: out.append(i)
+    return out
+    
+# Your code below! You should describe the problem and it will (hopefully) solve it
