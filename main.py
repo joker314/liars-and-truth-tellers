4fad27807079a9b3a9a7cce4b7823428e3964158
jokebookservice1 <jokebookyeye@gmail.com>
2018-12-22 17:43:09 +0200

    Rename vars & use list comprehension vs map()
    Use `binary` and `padded_binary` as this makes it clear what the difference between them is. Add some newlines to show some separation between them. Using `map` on a string was never intuitive, use a list comprehension instead.

--- main.py
+++ main.py
@@ -20,20 +20,22 @@ class Person():
 def list_all_solutions(num_of_people, conditions):
     solutions = []
     for decimal_combination in range(2 ** num_of_people): # Number of different combinations
-        bin_from_dec = bin(decimal_combination)[2:] # Strip the first 2 chars as these are '0b'
-        binary_combination = (num_of_people - len(bin_from_dec)) * "0" + bin_from_dec
+        binary = bin(decimal_combination)[2:] # Strip the first 2 chars as these are '0b'
+
+        padded_binary = (num_of_people - len(binary)) * "0" # Pad with zeros
+        padded_binary += binary # Actually add the binary
+
         all_conditions_satisfied_thus_far = True
         for check_condition in conditions:
             """
             We've now converted a combination into binary, e.g. 101
-            We can use map() to create a list [Person("1"), Person("0"), Person("1")]
+            We need to convert it to [Person("1"), Person("0"), Person("1")]
             Which will become a list of a T-teller, Liar, and T-teller, in that order
             """
-            if not check_condition(list(map(Person, binary_combination))):
+            if not check_condition([Person(c) for c in padded_binary]):
                all_conditions_satisfied_thus_far = False
                break
-        if all_conditions_satisfied_thus_far: solutions.append(binary_combination)
+        if all_conditions_satisfied_thus_far: solutions.append(padded_binary)
     return solutions
     
 # Your code below! You should describe the problem and it will (hopefully) solve it
-
