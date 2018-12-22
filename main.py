e8f82696397a374d7f809c9a63b089dbb23c2718
jokebookservice1 <jokebookyeye@gmail.com>
2018-12-22 17:35:20 +0200

    Maintain Python 3 support, make code more readable
    Add some comments, and use more intuitive variable names

--- main.py
+++ main.py
@@ -17,15 +17,22 @@ class Person():
     def __ne__(self, person):
         return self.am != person.am
 
-def list_all_solutions(num, keys):
+def list_all_solutions(num_of_people, conditions):
     solutions = []
-    for i in map(lambda x: (num + 2 - len(bin(x))) * "0" + bin(x)[2:], range(2**num)):
+    for decimal_combination in range(2 ** num_of_people): # Number of different combinations
+        bin_from_dec = bin(decimal_combination)[2:] # Strip the first 2 chars as these are '0b'
+        binary_combination = (num_of_people - len(bin_from_dec)) * "0" + bin_from_dec
         all_conditions_satisfied_thus_far = True
-        for j in keys:
-            if not j(map(Person, i)):
+        for check_condition in conditions:
+            """
+            We've now converted a combination into binary, e.g. 101
+            We can use map() to create a list [Person("1"), Person("0"), Person("1")]
+            Which will become a list of a T-teller, Liar, and T-teller, in that order
+            """
+            if not check_condition(list(map(Person, binary_combination))):
                all_conditions_satisfied_thus_far = False
                break
-        if all_conditions_satisfied_thus_far: solutions.append(i)
+        if all_conditions_satisfied_thus_far: solutions.append(binary_combination)
     return solutions
     
 # Your code below! You should describe the problem and it will (hopefully) solve it
