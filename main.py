8aa83c5206cbd06b08211f2a7bec1ce74138bb76
jokebookservice1 <jokebookyeye@gmail.com>
2018-12-22 18:24:51 +0200

    Remove the ability to pass lists of conditions
    This can instead be accomplished by using `and`

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Person():
     def __ne__(self, person):
         return self.am != person.am
 
-def list_all_solutions(num_of_people, conditions):
+def list_all_solutions(num_of_people, check_condition):
     solutions = []
     for decimal_combination in range(2 ** num_of_people): # Number of different combinations
         binary = bin(decimal_combination)[2:] # Strip the first 2 chars as these are '0b'
@@ -25,17 +25,13 @@ def list_all_solutions(num_of_people, conditions):
         padded_binary = (num_of_people - len(binary)) * "0" # Pad with zeros
         padded_binary += binary # Actually add the binary
 
-        all_conditions_satisfied_thus_far = True
-        for check_condition in conditions:
-            """
-            We've now converted a combination into binary, e.g. 101
-            We need to convert it to [Person("1"), Person("0"), Person("1")]
-            Which will become a list of a T-teller, Liar, and T-teller, in that order
-            """
-            if not check_condition([Person(c) for c in padded_binary]):
-               all_conditions_satisfied_thus_far = False
-               break
-        if all_conditions_satisfied_thus_far: solutions.append(padded_binary)
+        """
+        We've now converted a combination into binary, e.g. 101
+        We need to convert it to [Person("1"), Person("0"), Person("1")]
+        Which will become a list of a T-teller, Liar, and T-teller, in that order
+        """
+        if check_condition([Person(c) for c in padded_binary]):
+            solutions.append(padded_binary)
     return solutions
     
 # Your code below! You should describe the problem and it will (hopefully) solve it
